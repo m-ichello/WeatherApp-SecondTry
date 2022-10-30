b1107303271a1880e93c1529b53ff3c0421648a2
m-ichello <michelle.rheinheimer@gmail.com>
2022-10-30 18:59:01 +0100

    Update the time

--- src/app.js
+++ src/app.js
@@ -1,14 +1,31 @@
+function formatDate(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours < 10) {
+        hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+    let days = ["Sunday","Monday","Thursday","Wednesday","Tuesday","Friday","Saturday"];
+    let day = days[date.getDay()];
+    return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
     let descriptionElement=document.querySelector("#description");
     let humidityElement=document.querySelector("#humidity");
     let windElement=document.querySelector("#wind");
+    let dateElement=document.querySelector("#date");
     temperatureElement.innerHTML= Math.round(response.data.temperature.current);
     cityElement.innerHTML = response.data.city;
     descriptionElement.innerHTML = response.data.condition.description;
     humidityElement.innerHTML = response.data.temperature.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
+    dateElement.innerHTML = formatDate(response.data.time * 1000); 
 }
 
 
