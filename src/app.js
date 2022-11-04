b99c76421b68962539b0b52f2db4c13baf8c249e
m-ichello <michelle.rheinheimer@gmail.com>
2022-11-04 23:39:08 +0100

    Fixed formatDate/Day

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ function formatDate(timestamp) {
     return `${day} ${hours}:${minutes}`;
 }
 
-function formatDate(timestamp) {
+function formatDay(timestamp) {
     let date = new Date(timestamp * 1000);
     let day = date.getDay();
 
